0c3d611f3626c6dd3b9b2e6c253151567a18bf46
Md. Rubel Rana <33339942+MdRubelRana@users.noreply.github.com>
2019-01-12 00:05:15 +0600

    Add files via upload

---
+++ Beginner- Easy Problems/Add Them Up.cpp
@@ -0,0 +1,19 @@
+/*
+Read two integer variables, calculate their sum, and print it.
+
+Input:
+The input will contain two integers A and B (A, B < 20000000).
+
+Output:
+Print the sum of the two integers.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    cout << a + b << endl;
+return 0;
+}
