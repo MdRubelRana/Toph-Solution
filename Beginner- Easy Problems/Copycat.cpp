4bff353c144071cdb05973627c237b717f7fddae
Md. Rubel Rana <33339942+MdRubelRana@users.noreply.github.com>
2019-01-12 00:00:59 +0600

    Add files via upload

---
+++ Beginner- Easy Problems/Copycat.cpp
@@ -0,0 +1,19 @@
+
+/*Read an integer variable and print it.
+
+Input:
+The input will contain an integer A (A < 200000000).
+
+Output:
+Print the integer.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    cout << a << endl;
+return 0;
+}
